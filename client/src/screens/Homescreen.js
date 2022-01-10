48176f9832ddc4383487863d7046c0744deca47a
Md. Jewel Rana <m.jewelrana80@gmail.com>
2022-01-11 00:35:14 +0600

    Get Rooms To Home Screen

--- client/src/screens/Homescreen.js
+++ client/src/screens/Homescreen.js
@@ -1,26 +1,40 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios';
+import Room from '../components/Room';
 
 function Homescreen() {
 
     const [rooms, setRooms] = useState([])
+    const [loading, setLoading] = useState()
+    const [error, setError] = useState()
     
     useEffect(async() => {
         
         try {
+            setLoading(true);
             const data = (await axios.get('/api/rooms/getallrooms')).data
+
             setRooms(data)
+            setLoading(false);
+            
         } catch (error) {
+            setError(true)
             console.log(error)
+            setError(false)
         }
     }, [])
 
    
 
     return (
-        <div>
-            <h1>Home screens</h1>
-            <h1>there are {rooms.length} rooms</h1>
+        <div className="container">
+           <div className='row justify-content-center mt-5'>
+           {loading ? (<h1>Loading...</h1>) : error? (<h1>Error...</h1>) : (rooms.map( room =>{
+                return <div className='col-md-9 mt-2'>
+                    <Room room={room}/>
+                </div>
+            }))}
+           </div>
         </div>
     )
 }
