35be2ae4fe167808b9b9c59b628d073c786d78d6
jewel rana <m.jewelrana80@gmail.com>
2022-01-11 17:03:06 +0600

    User Authentication

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,18 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Bookingscreen from './screens/Bookingscreen';
 import Homescreen from './screens/Homescreen';
+import Loginscreen from "./screens/Loginscreen";
+import Registerscreen from "./screens/Registerscreen";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <BrowserRouter>
+        <Route path="/login" component={Loginscreen} />
+        <Route path="/register" component={Registerscreen} />
         <Route path="/home" exact component={Homescreen} />
-        <Route path="/book/:roomid" component={Bookingscreen}/>
+        <Route path="/book/:roomid" component={Bookingscreen} />
       </BrowserRouter>
     </div>
   );
--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const app = express();
 const dbconfig = require("./db");
 app.use(express.json());
 const roomsRoute = require("./routes/roomsRoute");
+const userRoute = require("./routes/userRoute");
 
 app.use("/api/rooms", roomsRoute);
+app.use("/api/users", userRoute);
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Example app listening on port - ${port}!`));
